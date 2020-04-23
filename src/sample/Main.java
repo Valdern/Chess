a18bf0fd483863934ecd29b7b8137a85143833a7
st256-ai <62150678+st256-ai@users.noreply.github.com>
2020-04-23 03:08:39 +0300

    Update Main.java
    New Chess board with a black pawn

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,34 +1,72 @@
 package sample;
 
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.RowConstraints;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.GridPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
+import java.io.InputStream;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 
 public class Main extends Application {
-
+    public static final double width = 60;
+    public static final double height = 60;
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception{
+        double x = 0;
+        double y = 0;
+        Group root = new Group();
+        GridPane tree = new GridPane();
 
-        Group group = new Group();
-        Scene scene = new Scene(group,480,480);
-
-        Parent content = FXMLLoader.load(getClass().getResource("/ChessBoard.fxml"));
-        BorderPane root = new BorderPane();
-        root.setCenter(content);
-        group.getChildren().add(root);
+        //creating a chessboard
+        Rectangle[][] rect = new Rectangle[8][8];
+        for( int i = 0; i < 8; i++){
+            tree.getColumnConstraints().add(new ColumnConstraints(60));
+            for(int j = 0; j < 8; j++) {
+                rect[i][j] = new Rectangle();
+                rect[i][j].setX(x);
+                rect[i][j].setY(y);
+                rect[i][j].setWidth(width);
+                rect[i][j].setHeight(height);
+                x = x + 60;
+                if ((i + j) % 2 == 0 ) {
+                    rect[i][j].setFill(Color.WHITE);
+                } else {
+                    rect[i][j].setFill(Color.GREY);
+                }
+                Group group = new Group(rect[i][j]);
+                root.getChildren().add(group);
+                tree.getRowConstraints().add(new RowConstraints(60));
+            }
+            y = y + 60;
+            x = 0;
+        }
         
+        //Setting a black pawn
+        Image img = new Image(getClass().getResourceAsStream("/resources/black_pawn.png"));
+        ImageView imageView = new ImageView(img);
+        imageView.setX(30);
+        imageView.setY(90);
+        root.getChildren().add(imageView);
+        imageView.setImage(img);
+        
+        
+        root.getChildren().add(tree);
+        tree.setGridLinesVisible(true);
+        Scene scene = new Scene(root,480,480);
         stage.setScene(scene);
         stage.setTitle("Chess");
         stage.show();
     }
 
-
 }
+
